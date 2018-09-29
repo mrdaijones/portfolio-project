f823fd2455ebd11fb523e44f6a8ce8b7cef157e9
Dai Jones <mrdaijones@yahoo.com>
2018-09-29 17:14:21 -0600

    test git commit from atom - blog in admin

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date= models.DateTimeField()
+    pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='')
