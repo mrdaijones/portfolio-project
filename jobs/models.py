a89fd62b2234058ee0f7baf86489772e82dcb2a6
Dai Jones <mrdaijones@yahoo.com>
2018-10-04 17:14:43 -0600

    Polishing

--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='')
     summary = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.summary
